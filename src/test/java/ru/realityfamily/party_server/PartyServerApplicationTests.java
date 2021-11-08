557c0524f08f677626a4fa83da765a8b22cab8b0
Леонид <leonis13579@gmail.com>
2021-11-08 14:58:40 +0300

    Init Proj

---
+++ src/test/java/ru/realityfamily/party_server/PartyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.realityfamily.party_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
